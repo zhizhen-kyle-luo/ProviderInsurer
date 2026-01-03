f15233ff1d6ee67ae42db44fa9d1a86e1c37809c
zhizhen-kyle-luo <zhizhen.luo07@gmail.com>
2026-01-03 03:24:31 -0500

    refactor: convert case files from Python to JSON format

--- src/data/cases/grey_zones/__init__.py
+++
@@ -1,8 +0,0 @@
-"""grey zone test cases for upcoding scenarios"""
-
-from .copd_respiratory_failure import COPD_RESPIRATORY_FAILURE_GREY, get_copd_respiratory_failure_grey
-
-__all__ = [
-    "COPD_RESPIRATORY_FAILURE_GREY",
-    "get_copd_respiratory_failure_grey",
-]
--- src/data/cases/post-acure/__init__.py
+++
@@ -1,7 +0,0 @@
-"""post-acute care cases (SNF, LTAC, IRF)"""
-
-from .snf_pulmonary_01 import SNF_PULMONARY_01
-from .snf_copd_02 import SNF_COPD_02
-from .snf_dialysis_03 import SNF_DIALYSIS_03
-
-__all__ = ["SNF_PULMONARY_01", "SNF_COPD_02", "SNF_DIALYSIS_03"]
